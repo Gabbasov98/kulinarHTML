23979fd20378c0fd5438d946138f8c7a4f99dd3a
Marsel Gabbasov <marsel13_98@mail.ru>
2021-10-05 09:46:39 +0300

    first commit

---
+++ modal.php
@@ -0,0 +1,102 @@
+<div class="modal fade cart-modal" id="addCartModal"  role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      
+      <div class="modal-body">
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">
+          <svg id="Слой_1" data-name="Слой 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16.65 16.67"><path fill="#000000" d="M297.14,420.79l-5.28-5.26c-.57-.58-1.14-1.15-1.72-1.72-.43-.44-.49-.85-.15-1.16s.71-.25,1.13.17l6.49,6.5.46.48c.21-.21.28-.26.39-.37l6.56-6.56a2,2,0,0,1,.35-.31.69.69,0,0,1,.95.93,2,2,0,0,1-.31.35q-3.28,3.29-6.56,6.56a3.17,3.17,0,0,0-.38.42c.41.37.28.24.41.37q3.3,3.27,6.56,6.56a1.15,1.15,0,0,1,.34.74c0,.19-.24.44-.44.56s-.56,0-.78-.22l-2.29-2.3-4.76-4.76c-.12.11-.24.21-.34.32l-6.57,6.56c-.08.08-.16.17-.24.24a.68.68,0,0,1-1,0,.69.69,0,0,1,0-1c.07-.08.16-.16.24-.24l6.53-6.53C296.85,421,296.74,421.15,297.14,420.79Z" transform="translate(-289.77 -412.45)"/></svg>
+
+          </span>
+        </button>
+        <div class="cart-modal__inner">
+          <img src="images/svg/cart_modal.svg" alt="">
+          <p class="cart-modal__title">Товар добавлен в корзину!</p>
+          <p class="cart-modal__desc">Может ещё что-то вас заинтересует?</p>
+          <div class="cart-modal__buttons">
+            <button type="button" data-dismiss="modal" aria-label="Close">продолжить выбор</button>
+            <button>оформить заказ</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="modal fade question-modal" id="questionModal"  role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      
+      <div class="modal-body">
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">
+          <svg id="Слой_1" data-name="Слой 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16.65 16.67"><path fill="#000000" d="M297.14,420.79l-5.28-5.26c-.57-.58-1.14-1.15-1.72-1.72-.43-.44-.49-.85-.15-1.16s.71-.25,1.13.17l6.49,6.5.46.48c.21-.21.28-.26.39-.37l6.56-6.56a2,2,0,0,1,.35-.31.69.69,0,0,1,.95.93,2,2,0,0,1-.31.35q-3.28,3.29-6.56,6.56a3.17,3.17,0,0,0-.38.42c.41.37.28.24.41.37q3.3,3.27,6.56,6.56a1.15,1.15,0,0,1,.34.74c0,.19-.24.44-.44.56s-.56,0-.78-.22l-2.29-2.3-4.76-4.76c-.12.11-.24.21-.34.32l-6.57,6.56c-.08.08-.16.17-.24.24a.68.68,0,0,1-1,0,.69.69,0,0,1,0-1c.07-.08.16-.16.24-.24l6.53-6.53C296.85,421,296.74,421.15,297.14,420.79Z" transform="translate(-289.77 -412.45)"/></svg>
+
+          </span>
+        </button>
+        <div class="question-modal__inner">
+            <p class="question-modal__title">Вопрос по товару</p>
+            <div class="question-modal__item">
+              <div class="question-modal__item-img">
+                <a href="#"><img src="images/catalog_item1.jpg" alt=""></a>
+              </div> 
+              <div class="question-modal__item-info">
+                <p class="question-modal__item-title"><a href="#">Форма для выпечки со съемным дном Caramella гранит круг 20 см</a> </p>
+                <p class="question-modal__item-desc">Код товара: 008244</p>
+              </div>
+            </div>
+            <div class="question-modal__inputs">
+              <div class="question-modal__name">
+                <label for="">Ваше имя <span>*</span></label>
+                <input type="text">
+              </div>
+              <div class="question-modal__email">
+                <label for="">Эл.почта <span>*</span></label>
+                <input type="email">
+              </div>
+              <div class="question-modal__phone" >
+                <label for="">Номер телефона</label>
+                <input type="tel" placeholder="+7 (     )      -">
+              </div>
+              <div class="question-modal__question">
+                <label for="">Ваш вопрос <span>*</span></label>
+                <textarea ></textarea>
+              </div>
+            </div>
+            
+            <button class="question-modal__btn">ОТПРАВИТЬ ВОПРОС</button>
+            <div class="question-modal__agree">
+              Нажимая кнопку “Отправить вопрос” вы принимаете <br>
+              <a href="">пользовательское соглашение на обработку персональных данных</a>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="modal fade message-modal" id="messageModal"  role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      
+      <div class="modal-body">
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">
+          <svg id="Слой_1" data-name="Слой 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16.65 16.67"><path fill="#000000" d="M297.14,420.79l-5.28-5.26c-.57-.58-1.14-1.15-1.72-1.72-.43-.44-.49-.85-.15-1.16s.71-.25,1.13.17l6.49,6.5.46.48c.21-.21.28-.26.39-.37l6.56-6.56a2,2,0,0,1,.35-.31.69.69,0,0,1,.95.93,2,2,0,0,1-.31.35q-3.28,3.29-6.56,6.56a3.17,3.17,0,0,0-.38.42c.41.37.28.24.41.37q3.3,3.27,6.56,6.56a1.15,1.15,0,0,1,.34.74c0,.19-.24.44-.44.56s-.56,0-.78-.22l-2.29-2.3-4.76-4.76c-.12.11-.24.21-.34.32l-6.57,6.56c-.08.08-.16.17-.24.24a.68.68,0,0,1-1,0,.69.69,0,0,1,0-1c.07-.08.16-.16.24-.24l6.53-6.53C296.85,421,296.74,421.15,297.14,420.79Z" transform="translate(-289.77 -412.45)"/></svg>
+
+          </span>
+        </button>
+        <div class="message-modal__inner">
+          <img src="images/svg/message_success.svg" alt="">
+          <p class="message-modal__title">Сообщение отправлено</p>
+          <p class="message-modal__desc">В ближайшее время мы свяжемся с вами.</p>
+          <div class="message-modal__buttons" type="button" data-dismiss="modal" aria-label="Close">
+            <button>ЗАКРЫТЬ СООБЩЕНИЕ</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
